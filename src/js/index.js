93c35b902111f75a5aa23055b293d1f1f46c7d45
Heather Conley <hconley6695@gmail.com>
2016-03-08 16:56:36 -0500

    starting to add jquery and javascript

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,9 @@ import $ from 'jquery';
 $('.box1').css('color', 'gray');
 $('.box2').css('color', 'gray');
 $('.box3').css('color', 'gray');
+
+//$('h2').addClass('#box3')
+
+$('#header3').on('click', function() {
+	$('#box3').addClass('.moveup');
+});
