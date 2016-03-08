e4dfca69137c11015511216eebbaa37a7830a672
Heather Conley <hconley6695@gmail.com>
2016-03-08 14:28:25 -0500

    set up boxes to be moved with header, paragraphs, dimensions, and styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 // Javascript Entry Point
 import $ from 'jquery';
 
-$(.box1).css('color', 'green');
+$('.box1').css('color', 'gray');
+$('.box2').css('color', 'gray');
+$('.box3').css('color', 'gray');
